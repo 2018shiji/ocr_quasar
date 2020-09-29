09efde4939e58d21f82de6f0a0e7dbb1c5de690d
lizhuangjie.chnet <lizhuangjie.chnet@szcmml.com>
2020-09-29 18:16:16 +0800

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,113 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <q-tree
+          :nodes="simple"
+          selected-color="blue"
+          node-key="label"
+          :selected.sync="selected">
+        </q-tree>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false,
+      selected: '',
+      simple: [
+        {
+          label: 'Satisfied customers0',
+          children: [
+            {
+              label: 'Good food0',
+              children: [
+                { handler: (node) => this.handleClick(node), label: 'quality ingredients0', url: 'role' },
+                { handler: (node) => this.handleClick(node), label: 'Good recipe0', url: 'test' }
+              ]
+            },
+            {
+              label: 'Good service'
+            }
+          ]
+        },
+        {
+          label: 'Satisfied customers1',
+          children: [
+            {
+              label: 'Good food1',
+              children: [
+                { handler: (node) => this.handleClick(node), label: 'quality ingredients1', url: 'role' },
+                { handler: (node) => this.handleClick(node), label: 'Good recipe1', url: 'test' }
+              ]
+            },
+            {
+              label: 'Good service1'
+            }
+          ]
+        },
+        {
+          label: 'Satisfied customers2',
+          children: [
+            {
+              label: 'Good food2',
+              children: [
+                { handler: (node) => this.handleClick(node), label: 'quality ingredients2', url: 'role' },
+                { handler: (node) => this.handleClick(node), label: 'Good recipe2', url: 'test' }
+              ]
+            },
+            {
+              label: 'Good service2'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    handleClick (node) {
+      console.log(`node: ${JSON.stringify(node)}`)
+      this.$router.push(node.url)
+    }
+  }
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: 'index', component: () => import('pages/Index.vue') },
+      { path: 'role', component: () => import('layouts/Role.vue') },
+      { path: 'test', component: () => import('pages/Index.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
